1ce8069fd4e08338e89ed8b777e4649206c8ea12
CTII <clay.thompson13@gmail.com>
2023-06-06 22:29:45 -0500

    🔧 Add OpenScale and HX711 library to OpenScale
    - Define calibration factor and setting_tare_point.

--- Firmware.ino
+++ Firmware.ino
@@ -1,6 +1,21 @@
 #include <Wire.h>
+#include <OpenScale.h>
+#include <HX711.h>
+
+
+// Define the calibration factor for your load sensor
+const float CALIBRATION_FACTOR = -1760;
+
+long setting_tare_point; //Zero value that is found when scale is tared
+const byte statusLED = 13;  //Flashes with each reading
+
+
 
 Void setup() {
+
+    Wire.setSpeed(100000); //Default 100,000 Hz
+    Wire.begin(); 
+    //Setup UART connection
     Serial1.begin(9600);
 }
 
